2f2fbd5ba1683fa4d9fec7d388b462838eb2e541
Paul Dziemianowicz <Paul@Pauls-MacBook-Pro.local>
2016-05-20 10:07:37 -0600

    initial client-side setup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/yoda', function(req, res, next) {
+  res.json({yoda: "lyrics"});
+});
+
+
+// router.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
+// .header("X-Mashape-Key", "14NxsjwsZMmshIVvFwDV9UKvwgcjp1ZcceSjsneDQPbZO7FD62")
+// .header("Accept", "text/plain")
+// .then(function (result) {
+//   console.log(result.body);
+// });
+
 module.exports = router;
