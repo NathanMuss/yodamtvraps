7a2360b8727f100dea0e847033d49be92ee398c2
Nathan Musselman <nathanmuss@gmail.com>
2016-05-20 15:45:33 -0600

    this is a commit message

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
+  var yodaTweet;
 
     $('#tweetRap').on('click', function() {
+
+      $(document)
       $.get( "/yoda", function( data ) {
-      $( ".result" ).html( data );
-      
+        yodaTweet = data;
+      $( ".result" ).html( yodaTweet );
+      $.getJSON
+      (
+        "http://vaas.acapela-group.com/Services/UrlMaker?jsoncallback=?",
+        {
+          cl_login: 'EVAL_VAAS',
+          cl_app: 'EVAL_8195604',
+          cl_pwd: '3mkr1ggk',
+          req_voice: "willlittlecreature22k",
+          req_text: yodaTweet,
+        },
+        function(data){
+          $(".player").html(
+            "<audio src='"+data.snd_url+"' autoplay />"
+          )
+        }
+      )
       console.log(data);
     })
   });
-
 });
